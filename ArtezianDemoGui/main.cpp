71662c12b8c8b4e39a99e35f1608517098363065
evgeny <asteroidsuper@mail.ru>
2017-05-08 15:00:09 +0700

    renamed QApllication inmain func

--- ArtezianDemoGui/main.cpp
+++ ArtezianDemoGui/main.cpp
@@ -6,15 +6,17 @@
 
 int main(int argc, char *argv[])
 {
-	QApplication a(argc, argv);
+	QApplication application(argc, argv);
 	try
 	{
 		MainWindow w;
 		w.show();
-		return a.exec();
+		return application.exec();
 	}
 	catch(const std::runtime_error& exp)
 	{
 		QMessageBox::critical(nullptr, u8"Ошибка при инициализации: ", exp.what());
+
+		return -1;
 	}
 }
